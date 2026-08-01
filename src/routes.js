e106ed9f9fbec62aedb60e1a3e186f6524e86790
Ebigide Jude <judmc.26@gmail.com>
2026-08-01 22:45:06 +0100

    add edit organization form with update model, controller, and routes

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ import { showOrganizationDetailsPage } from './controllers/organizations.js';
 import { showNewOrganizationForm } from './controllers/organizations.js';
 import { processNewOrganizationForm } from './controllers/organizations.js';
 import { organizationValidation } from './controllers/organizations.js';
+import { showEditOrganizationForm } from './controllers/organizations.js';
+import { processEditOrganizationForm } from './controllers/organizations.js';
 import { showProjectsPage, showProjectDetailsPage } from './controllers/projects.js';
 import { showCategoriesPage, showCategoryDetailsPage } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
@@ -22,6 +24,12 @@ router.get('/new-organization', showNewOrganizationForm);
 
 // Route to handle new organization form submission
 router.post('/new-organization', organizationValidation, processNewOrganizationForm);
+
+// Route for edit organization page
+router.get('/edit-organization/:id', showEditOrganizationForm);
+
+// Route to handle edit organization form submission
+router.post('/edit-organization/:id', organizationValidation, processEditOrganizationForm);
 
 // Route for organization details page
 router.get('/organization/:id', showOrganizationDetailsPage);
